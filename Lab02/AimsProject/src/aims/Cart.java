3804f6acbaf6130aafa68de597eaf6de41251ea9
noeruchangd <haiduongnguyen32@gmail.com>
2024-10-22 17:39:57 +0700

    initial commit

---
+++ Lab02/AimsProject/src/aims/Cart.java
@@ -0,0 +1,41 @@
+package aims;
+
+public class Cart {
+	private int qtyOrdered;
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = 
+			new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	
+    public Cart() {
+        qtyOrdered = 0;
+    }
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
+			System.out.println("Disc " + disc.getTitle() + " added successfully!");
+		}
+		else System.out.println("Cart is full!");
+	}
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		for (int i = 0; i < qtyOrdered; i++) {
+			if (itemsOrdered[i].getTitle().equals(disc.getTitle()));
+			for (int j = i; j < qtyOrdered - 1; j++) {
+				itemsOrdered[j] = itemsOrdered[j + 1];
+			}
+			itemsOrdered[qtyOrdered - 1] = null;
+			qtyOrdered--;
+			System.out.println("Disc " + disc.getTitle() + " removed successfully!");
+			return;
+		}
+		System.out.println("Disc " + disc.getTitle() + "not found!");
+	}
+	public float totalCost() {
+		float sum = 0;
+		for (int i = 0; i < qtyOrdered; i++) {
+			sum = sum + itemsOrdered[i].getCost();
+		}
+		return sum;
+	}
+}
---
+++ Lab02/AimsProject/src/aims/DigitalVideoDisc.java
@@ -0,0 +1,56 @@
+package aims;
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    } // all params
+    
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+        this.category = ""; 
+        this.director = ""; 
+        this.length = 0;  
+        this.cost = 0.0f;  
+    } // only title
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = ""; 
+        this.length = 0;    
+        this.cost = cost;
+    } // only cat, title and cost
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = 0;   
+        this.cost = cost;
+    } // all but length
+	
+}
