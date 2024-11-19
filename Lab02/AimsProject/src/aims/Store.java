be35a03451a202bfc6e1907ec2723d14ce96da9c
noeru <haiduongnguyen32@gmail.com>
2024-11-19 22:13:58 +0700

    Added store code

---
+++ Lab02/AimsProject/src/aims/Store.java
@@ -0,0 +1,33 @@
+package aims;
+
+public class Store {
+	private int stockQuantity = 0;
+	private static final int MAX_STOCK = 100;
+	private DigitalVideoDisc itemsinStore[] = 
+			new DigitalVideoDisc[MAX_STOCK];
+
+	public Store() {
+		stockQuantity = 0;
+	}
+	public void addDVD(DigitalVideoDisc dvd) {
+		if (stockQuantity < MAX_STOCK) {
+			itemsinStore[stockQuantity] = dvd;
+			stockQuantity++;
+			System.out.println("Disc " + dvd.getTitle() + " added to stock!");
+		}
+		else System.out.println("No more space in inventory!");
+	}
+	public void removeDVD(DigitalVideoDisc dvd) {
+		for (int i = 0; i < stockQuantity; i++) {
+			if (itemsinStore[i].getTitle().equals(dvd.getTitle()));
+			for (int j = i; j < stockQuantity - 1; j++) {
+				itemsinStore[j] = itemsinStore[j + 1];
+			}
+			itemsinStore[stockQuantity - 1] = null;
+			stockQuantity--;
+			System.out.println("Disc " + dvd.getTitle() + " removed from stock!");
+			return;
+		}
+		System.out.println("Disc " + dvd.getTitle() + "not found!");
+	}
+}
