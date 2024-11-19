842b54c2dd2316aa2d9a512606919fdd5db9e60a
noeru <haiduongnguyen32@gmail.com>
2024-11-19 23:38:14 +0700

    concat

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,7 +1,13 @@
 package hust.soict.dsai.aims;
 
+import hust.soict.dsai.test.cart.CartTest;
+import hust.soict.dsai.test.disc.StoreTest;
+import hust.soict.dsai.test.store.TestPassingParameter;
+
 public class Aims {
 	public static void main(String[] args) {
-
+		CartTest.main(args);
+		StoreTest.main(args);
+		TestPassingParameter.main(args);
 	}
 }
