2fc842d04aeafc97ec11281a0b2596b75837312b
noeru <haiduongnguyen32@gmail.com>
2024-12-19 17:07:10 +0700

    implementations

--- GUIProject/src/hust/soict/dsai/javafx/Painter.java
+++ GUIProject/src/hust/soict/dsai/javafx/Painter.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 public class Painter extends Application {
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("hust/soict/program/javafx/Painter.fxml"));
+		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("hust/soict/dsai/javafx/Painter.fxml"));
 		
 		Scene scene = new Scene(root);
 		stage.setTitle("Painter");
